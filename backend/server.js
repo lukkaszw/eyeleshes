1945c3f5853b46aaccb7a123ea112de38f512bd2
Lukasz Wojdat <lukaszwojdat88@gmail.com>
2020-09-16 17:29:48 +0200

    Add basic server configuration

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const compression = require('compression');
+const mongoSanitize = require('express-mongo-sanitize');
 require('./database');
 
 const port = process.env.PORT || 8000;
 
 const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(compression());
+app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(mongoSanitize());
+
+app.get('*', (req, res) => {                       
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));                               
+});
 
 app.listen(() => {
   console.log(`Server is litening on port ${port}.`);
